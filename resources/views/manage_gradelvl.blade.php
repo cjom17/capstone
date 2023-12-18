cf0cba24f3cfa28ddd962c91e33598198aef887f
cjom17 <nztechonophile.1@gmail.com>
2023-12-18 08:36:21 +0800

    added

--- resources/views/manage_gradelvl.blade.php
+++ resources/views/manage_gradelvl.blade.php
@@ -335,7 +335,7 @@
 							</div> -->
 							<nav aria-label="breadcrumb" role="navigation">
 								<ol class="breadcrumb">
-									<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+									<li class="breadcrumb-item">Home</li>
 									<li class="breadcrumb-item active" aria-current="page">Manage Grade Levels</li>
 								</ol>
 							</nav>
--- resources/views/teacher_reset_password.blade.php
+++ resources/views/teacher_reset_password.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="{{ asset('img/image (5).png') }}" />
-    <title>Barangay Connect</title>
+    <title>Boljoon National High School</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
